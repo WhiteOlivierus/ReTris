406683ca1522a5d07fdf8458a9ae681fd3d55dcf
WhiteOlivierus <bas.dijkstra@student.hku.nl>
2018-09-12 20:16:30 +0200

    Bezig met UI, nog niet helemaal done

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Counter : MonoBehaviour {
+
+
+    public void AddOne (int max = 99) {
+        int a;
+        Int32.TryParse (GetComponent<Text> ().text, out a);
+        if (a < max)
+            a += 1;
+        GetComponent<Text> ().text = a.ToString ();
+    }
+
+
+    public void RemoveOne (int min = 1) {
+        int r;
+        Int32.TryParse (GetComponent<Text> ().text, out r);
+        if (r > min)
+            r -= 1;
+        GetComponent<Text> ().text = r.ToString ();
+    }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour {
+
+
+    public int points = 0;
+    public int playerCount = 1;
+    public int fieldHeight = 10;
+    public int fieldWidth = 10;
+
+    private List<string> properties;
+    private List<string> playersInput;
+
+
+    void Awake () {
+        DontDestroyOnLoad (gameObject);
+    }
+
+
+    public void StartGame () {
+        GetInitData ();
+        print ("properties: " + properties.Count);
+        print ("properties: " + playersInput.Count);
+        SceneManager.LoadScene (1);
+    }
+
+
+    private void GetInitData () {
+        GameObject [] go = GameObject.FindGameObjectsWithTag ("UI Data");
+
+        foreach (GameObject g in go) {
+            if (g.transform.parent.name == "Amount") {
+                properties.Add (g.GetComponent<Text> ().text);
+            } else {
+                playersInput.Add (g.GetComponent<Text> ().text);
+            }
+        }
+    }
+
+
+    public void EndGame () {
+
+    }
+}
