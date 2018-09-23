7bcfc37999aebde5f6509ac3a5f7b306107e8fb0
WhiteOlivierus <bas.dijkstra@student.hku.nl>
2018-09-23 02:09:03 +0200

    Fixed the point system

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,7 +8,6 @@ public class Controller : MonoBehaviour {
 	public KeyCode keyRotate;
 
 
-<<<<<<< HEAD
 	void Start () {
 		DontDestroyOnLoad (gameObject);
 	}
@@ -19,12 +18,13 @@ public class Controller : MonoBehaviour {
 		keyRight = keys [1];
 		keyDown = keys [2];
 		keyRotate = keys [3];
-=======
+	}
+
+
 	public Controller (KeyCode l, KeyCode r, KeyCode d, KeyCode t) {
 		keyLeft = l;
 		keyRight = r;
 		keyDown = d;
 		keyRotate = t;
->>>>>>> 01444ac3ea8974ce2a32770543a4d8d8759fa340
 	}
 }
